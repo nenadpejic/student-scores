39f47941d4b5da8962ff241d7e7c2ebf6d2e71bf
nenadpejic <npejic89@gmail.com>
2021-01-16 19:24:53 +0100

    Implemented Error handling with try with

--- Program.fs
+++ Program.fs
@@ -5,12 +5,25 @@ let main argv =
     if argv |> Array.length = 1 then
         let filePath = argv.[0]
         if System.IO.File.Exists filePath then
-            printfn "Processing file: %s" filePath
-            Student.handleStudents filePath
-            0
+            try
+                printfn "Processing file: %s" filePath
+                Student.handleStudents filePath
+                0
+            with
+            | :? System.FormatException as e ->
+                printfn "Error: %s" e.Message
+                printfn "File was not in the expected format"
+                1
+            | :? System.IO.IOException as e ->
+                printfn "Error: %s" e.Message
+                printfn "File is in use by another program"
+                2
+            | _ as e ->
+                printfn "Unexpected error: %s" e.Message
+                3
         else
             printfn "File path: %s, does not exist" filePath
-            2
+            4
     else
         printfn "Please enter file path"
-        1
+        5
